a37ab5a9ecf4db6bbb84ce06c9a3e73872d9aaaf
Gabe SantaCruz <gsantacruz7430@gmail.com>
2026-07-18 17:40:19 -0700

    Fix mobile attribution migration

--- drizzle/0023_mobile_creator_attribution.sql
+++ drizzle/0023_mobile_creator_attribution.sql
@@ -29,7 +29,7 @@ ALTER TABLE "mobile_creator_attributions" ADD CONSTRAINT "mobile_creator_attribu
 --> statement-breakpoint
 ALTER TABLE "mobile_creator_attributions" ADD CONSTRAINT "mobile_creator_attributions_first_click_id_creator_attribution_clicks_id_fk" FOREIGN KEY ("first_click_id") REFERENCES "public"."creator_attribution_clicks"("id") ON DELETE cascade ON UPDATE no action;
 --> statement-breakpoint
-ALTER TABLE "mobile_creator_attributions" ADD CONSTRAINT "mobile_creator_attributions_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE set null ON UPDATE no action;
+ALTER TABLE "mobile_creator_attributions" ADD CONSTRAINT "mobile_creator_attributions_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE set null ON UPDATE no action;
 --> statement-breakpoint
 CREATE INDEX "mobile_creator_attributions_link_id_idx" ON "mobile_creator_attributions" USING btree ("tracking_link_id");
 --> statement-breakpoint
